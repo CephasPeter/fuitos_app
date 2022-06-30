96bea2af4f0d66920712ae274ca6b07f067fec0a
CephasPeter <52112132+CephasPeter@users.noreply.github.com>
2022-06-30 09:07:23 +0100

    Working on Onboarding pages

---
+++ lib/screens/onboarding/onboarding_home.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class OnboardingHome extends StatefulWidget {
+  const OnboardingHome({Key? key}) : super(key: key);
+
+  static const String route = "/onboardingHome";
+
+  @override
+  State<OnboardingHome> createState() => _OnboardingHomeState();
+}
+
+class _OnboardingHomeState extends State<OnboardingHome> {
+
+  @override
+  Widget build(BuildContext context) {
+    final Size size = MediaQuery.of(context).size;
+    double multiplier = 25 * size.height * 0.01;
+    final double imageHeight = size.height * 0.5;
+    final double imageWidth = size.width * 0.6;
+
+    return AnnotatedRegion<SystemUiOverlayStyle>(
+      value: const  SystemUiOverlayStyle(
+          statusBarColor: AppColor.background,
+          statusBarIconBrightness: Brightness.dark,
+          statusBarBrightness: Brightness.dark,
+          systemNavigationBarColor: AppColor.background,
+          systemNavigationBarIconBrightness: Brightness.dark
+      ),
+      child: Column(),
+    );
+  }
+}
